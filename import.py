beb8aab9e2c0dca5e607fc1b24576efc7e7b2b1d
Blake <45582195+blakely4206@users.noreply.github.com>
2020-03-30 20:43:27 -0500

    Delete import.py

--- import.py
+++
@@ -1,8 +0,0 @@
-f = open("books.csv")
-    reader = csv.reader(f)
-    for isbn, title, author, year in reader:
-        db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
-                   {"isbn": isbn, "title": title, "author": author, "year": year})
-                   
-    
-    db.commit()
